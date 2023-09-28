d36c96267950dfe6aa276601bc8fd0ea03e2a217
Samuel Borraccini <samuel@MacBook-Air-de-Samuel.local>
2023-09-28 11:59:22 -0300

    full commit 4

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -13,11 +13,16 @@ const ContactSection = () => {
   };
   return (
     <>
-      <div id="contact" className="flex justify-between items-top px-20 mb-4">
+      <div
+        id="contact"
+        className="flex flex-col gap-6 lg:gap-0 lg:justify-between items-top px-20 mb-4"
+      >
         <div className="relative">
-          <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-500 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute -top-1 right-20 transform -translate-x-1/2 -translate-1/2"></div>
-          <h2 className="text-4xl font-bold text-white mb-4">Contactame</h2>
-          <p className="text-[#ADB7BE] flex gap-2">
+          <div className=" lg:bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-500 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute -top-1 right-20 transform -translate-x-1/2 -translate-1/2"></div>
+          <h2 className="lg:text-4xl font-bold text-xl text-white mb-4">
+            Contactame
+          </h2>
+          <p className="text-[#ADB7BE] lg:text-lg text-sm flex gap-2">
             SamuelBorraccini@gmail.com
             <Image
               src={copy ? "/icons/tick.svg" : "/icons/copy.svg"}
@@ -29,20 +34,22 @@ const ContactSection = () => {
           </p>
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-white mb-4">Mis Redes</h2>
-          <p className="text-[#ADB7BE] max-w-md mb-6">
+          <h2 className="lg:text-4xl text-xl font-bold text-white mb-4">
+            Mis Redes
+          </h2>
+          <p className="text-[#ADB7BE] max-w-md lg:text-lg text-sm mb-6">
             Actualmente estoy buscando oportunidades laborales, me puedes
             contactar mediante mi correo electronico o mis redes de trabajo que
             aparecen a continuacion. Me comunicare devuelta lo mas pronto
             posible!
           </p>
-          <div className="flex gap-6">
+          <div className="flex gap-2 md:gap-6">
             <Link
               href={"https://github.com/samuelborraccini/samuelborraccini"}
               target="blank"
             >
               <Image
-                className="hover:shadow-slate-500/50 shadow-lg rounded-full duration-200"
+                className="hover:shadow-slate-500/50 shadow-lg rounded-full duration-300"
                 src={"/icons/github.svg"}
                 width={32}
                 height={32}
@@ -51,13 +58,12 @@ const ContactSection = () => {
             <Link
               href={"https://www.linkedin.com/in/samuel-borraccini-939146248/"}
               target="blank"
-              className="shadow-white"
             >
               <Image
                 src={"/icons/linkedIn.svg"}
                 width={32}
                 height={32}
-                className="hover:shadow-blue-500/50 shadow-lg rounded-full duration-200"
+                className="hover:shadow-blue-500/50 shadow-lg hover:shadow-lg rounded-full duration-300"
               />
             </Link>
           </div>
